60ad043cfd9ec86c9825a087b7900c081c6ee7ec
Xavier Durand-Smet <xavier.durand-smet>
2017-05-03 09:58:27 +0200

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+import TodoApp from 'TodoApp';
+
+// Load foundation
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('app')
+);
---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TodoApp = React.createClass({
+    render () {
+        return (
+            <div>
+                TodoApp.jsx
+            </div>
+        ) 
+    }
+})
+
+export default TodoApp;
